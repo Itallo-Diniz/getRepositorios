b5a943d0e8f198f5e71bfbc8769e76e90a236003
Itallo-Diniz <56324908+Itallo-Diniz@users.noreply.github.com>
2020-07-06 22:50:28 -0300

    Add files via upload

---
+++ getRepositorios.js
@@ -0,0 +1,30 @@
+
+function buscar() {
+    var user = document.querySelector('input').value;
+    var listas = document.querySelector('ul');
+    listas.innerHTML = '';
+    var carregando = document.createElement('li');
+    var carregandoText = document.createTextNode('Carregando...');
+    carregando.appendChild(carregandoText);
+    listas.appendChild(carregando);
+    axios.get('http://api.github.com/users/'+ user +'/repos')
+    .then(function(response){       
+        listas.innerHTML = '';
+        response.data.forEach(element => {
+            var newList = document.createElement('li');
+            var conteudo = element.name + '. Url: ' + element.html_url;
+            var repoName = document.createTextNode(conteudo);
+            newList.appendChild(repoName);
+            listas.appendChild(newList);
+            
+        });
+    })
+    .catch(function(error){
+        listas.innerHTML = '';
+        alert(error + ': Usuário não encontrado.');
+    });
+    
+}
+
+
+
